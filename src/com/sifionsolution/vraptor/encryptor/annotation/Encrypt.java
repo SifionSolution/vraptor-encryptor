c3840f8c3f37f11783724948a667ebc0d1beefe0
Marco <mtnoronha7@gmail.com>
2014-08-11 14:16:05 -0300

    Encrypt Strategy can be specified

--- src/com/sifionsolution/vraptor/encryptor/annotation/Encrypt.java
+++ src/com/sifionsolution/vraptor/encryptor/annotation/Encrypt.java
@@ -6,7 +6,10 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import com.sifionsolution.vraptor.encryptor.EncryptStrategy;
+
 @Retention(RUNTIME)
 @Target(PARAMETER)
 public @interface Encrypt {
+	EncryptStrategy[] value();
 }
