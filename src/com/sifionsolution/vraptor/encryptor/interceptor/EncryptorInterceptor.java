830a0996dbe68945c73900f364daf3f867d15141
Marco <mtnoronha7@gmail.com>
2014-08-11 14:16:34 -0300

    Parameter change in debug mode

--- src/com/sifionsolution/vraptor/encryptor/interceptor/EncryptorInterceptor.java
+++ src/com/sifionsolution/vraptor/encryptor/interceptor/EncryptorInterceptor.java
@@ -1,5 +1,7 @@
 package com.sifionsolution.vraptor.encryptor.interceptor;
 
+import static com.sifionsolution.commons.ContentVerifyer.notEmpty;
+
 import java.lang.annotation.Annotation;
 
 import javax.enterprise.context.RequestScoped;
@@ -14,7 +16,10 @@ import br.com.caelum.vraptor.http.ValuedParameter;
 import br.com.caelum.vraptor.interceptor.SimpleInterceptorStack;
 import br.com.caelum.vraptor.observer.ParametersInstantiator;
 
+import com.sifionsolution.vraptor.encryptor.EncryptStrategy;
+import com.sifionsolution.vraptor.encryptor.Encryptor;
 import com.sifionsolution.vraptor.encryptor.annotation.Encrypt;
+import com.sifionsolution.vraptor.encryptor.implementation.Sha512Encryptor;
 
 @Intercepts(after = ParametersInstantiator.class)
 @RequestScoped
@@ -39,14 +44,38 @@ public class EncryptorInterceptor {
 	public void around(SimpleInterceptorStack stack) {
 		ValuedParameter[] valuedParameters = methodInfo.getValuedParameters();
 
-		for (ValuedParameter obj : valuedParameters) {
-			if (obj.getParameter().isAnnotationPresent(Encrypt.class)) {
-				String value = String.valueOf(obj.getValue());
-				System.out.println(value);
+		for (int i = 0; i < valuedParameters.length; i++) {
+			ValuedParameter obj = valuedParameters[i];
+
+			Annotation annotation = obj.getParameter().getAnnotation(Encrypt.class);
+
+			if (annotation == null) {
+				continue;
 			}
+
+			EncryptStrategy[] strategyArray = ((Encrypt) annotation).value();
+
+			Encryptor encryptor = extractEncryptor(strategyArray);
+
+			String value = String.valueOf(obj.getValue());
+			System.out.println("plain: ");
+			System.out.println(value);
+			System.out.println("cript: ");
+			String encrypted = encryptor.encrypt(value);
+			System.out.println(encrypted);
+
+			methodInfo.setParameter(i, encrypted);
 		}
 
 		stack.next();
+	}
+
+	private Encryptor extractEncryptor(EncryptStrategy[] strategyArray) {
+		if (notEmpty(strategyArray)) {
+			return strategyArray[0].getEncryptor();
+		}
+
+		return new Sha512Encryptor();
 	}
 
 	@Accepts
