7e9aa9e745d6b2b4f2c115baae7a38d2ef4d8155
Marco <mtnoronha7@gmail.com>
2014-08-11 11:43:17 -0300

    Test object

---
+++ test/com/sifionsolution/vraptor/encryptor/MyController.java
@@ -0,0 +1,19 @@
+package com.sifionsolution.vraptor.encryptor;
+
+import com.sifionsolution.vraptor.encryptor.annotation.Encrypt;
+
+public class MyController {
+
+	public void first(@Encrypt String password, Integer number, Object obj) {
+	}
+
+	public void middle(Integer number, @Encrypt String password, Object obj) {
+	}
+
+	public void last(Object obj, Integer number, @Encrypt String password) {
+	}
+
+	public void unused(String whatever, Object obj) {
+	}
+
+}
