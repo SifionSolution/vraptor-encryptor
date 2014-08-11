316c48d811e85bb031813bde7b731a1e4c81ddea
Marco <mtnoronha7@gmail.com>
2014-08-11 13:34:10 -0300

    Default Encryptor strategy

---
+++ src/com/sifionsolution/vraptor/encryptor/Encryptor.java
@@ -0,0 +1,10 @@
+package com.sifionsolution.vraptor.encryptor;
+
+import com.sifionsolution.vraptor.encryptor.salter.EncryptSalter;
+
+public interface Encryptor {
+	public String encrypt(String plain);
+
+	public String encrypt(String plain, EncryptSalter salter);
+
+}
---
+++ src/com/sifionsolution/vraptor/encryptor/implementation/Sha512Encryptor.java
@@ -0,0 +1,29 @@
+package com.sifionsolution.vraptor.encryptor.implementation;
+
+import com.google.common.hash.Hashing;
+import com.sifionsolution.vraptor.encryptor.Encryptor;
+import com.sifionsolution.vraptor.encryptor.salter.EncryptSalter;
+import com.sifionsolution.vraptor.encryptor.salter.implementation.ShuffleSalter;
+
+public class Sha512Encryptor implements Encryptor {
+
+	@Override
+	public String encrypt(String plain) {
+		return encrypt(plain, new ShuffleSalter());
+	}
+
+	@Override
+	public String encrypt(String plain, EncryptSalter salter) {
+		if (salter == null)
+			throw new IllegalArgumentException("The salter cannot be null.");
+
+		final String encrypted = toSha512(salter.salt(plain));
+		String encryptedTwice = toSha512(salter.salt(encrypted));
+
+		return encryptedTwice;
+	}
+
+	private String toSha512(String salt) {
+		return Hashing.sha512().hashUnencodedChars(salt).toString();
+	}
+}
