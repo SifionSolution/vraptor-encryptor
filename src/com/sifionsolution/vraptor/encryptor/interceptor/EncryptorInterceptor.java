01f8f62c478b898c2b10a173ba5b3e20d391d3f2
Marco <mtnoronha7@gmail.com>
2014-08-11 19:52:36 -0300

    Adding logger, removing after

--- src/com/sifionsolution/vraptor/encryptor/interceptor/EncryptorInterceptor.java
+++ src/com/sifionsolution/vraptor/encryptor/interceptor/EncryptorInterceptor.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Annotation;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.caelum.vraptor.Accepts;
 import br.com.caelum.vraptor.AroundCall;
 import br.com.caelum.vraptor.Intercepts;
@@ -14,18 +17,19 @@ import br.com.caelum.vraptor.controller.ControllerMethod;
 import br.com.caelum.vraptor.core.MethodInfo;
 import br.com.caelum.vraptor.http.ValuedParameter;
 import br.com.caelum.vraptor.interceptor.SimpleInterceptorStack;
-import br.com.caelum.vraptor.observer.ParametersInstantiator;
 
 import com.sifionsolution.vraptor.encryptor.EncryptStrategy;
 import com.sifionsolution.vraptor.encryptor.Encryptor;
 import com.sifionsolution.vraptor.encryptor.annotation.Encrypt;
 import com.sifionsolution.vraptor.encryptor.implementation.Sha512Encryptor;
 
-@Intercepts(after = ParametersInstantiator.class)
+@Intercepts
 @RequestScoped
 public class EncryptorInterceptor {
 
 	private MethodInfo methodInfo;
+
+	private static final Logger logger = LoggerFactory.getLogger(EncryptorInterceptor.class);
 
 	/*
 	 * CDI eyes only
@@ -42,6 +46,7 @@ public class EncryptorInterceptor {
 
 	@AroundCall
 	public void around(SimpleInterceptorStack stack) {
+		logger.info("EncrytorInterceptor activated");
 		ValuedParameter[] valuedParameters = methodInfo.getValuedParameters();
 
 		for (int i = 0; i < valuedParameters.length; i++) {
@@ -52,6 +57,8 @@ public class EncryptorInterceptor {
 			if (annotation == null) {
 				continue;
 			}
+
+			logger.info("Intercepting parameter name: " + obj.getParameter().getName());
 
 			EncryptStrategy[] strategyArray = ((Encrypt) annotation).value();
 
@@ -80,6 +87,8 @@ public class EncryptorInterceptor {
 
 	@Accepts
 	public boolean accepts(ControllerMethod method) {
+		System.out.println("===================> SHOULD I ?");
+		logger.info("Should I accept?");
 		Annotation[][] annotations = method.getMethod().getParameterAnnotations();
 		for (Annotation[] ann : annotations) {
 			for (Annotation a : ann) {
